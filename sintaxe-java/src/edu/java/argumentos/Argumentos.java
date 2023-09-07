208b79288e82c293ae387f390f79c25718bc5fee
inNN-Abner <abner.faculdade440@gmail.com>
2023-09-07 14:59:55 -0300

    Usando o Scanner para receber argumentos

--- sintaxe-java/src/edu/java/argumentos/Argumentos.java
+++ sintaxe-java/src/edu/java/argumentos/Argumentos.java
@@ -1,4 +1,6 @@
 package edu.java.argumentos;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Argumentos {
     public static void main(String[] args) {
@@ -8,8 +10,23 @@ public class Argumentos {
         int idade = Integer.valueOf(args[2]); //Definindo variável int dentro da String
         double altura = Double.valueOf(args[3]); //Definindo variável double dentro da String
 
-        System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
-        System.out.println("Eu tenho " + idade + " anos de idade.");
-        System.out.println("Minha altura é " + altura + "cm");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        //Scanner recebe argumento do usuário
+        //System in define que recebe pelo teclado
+        //Use Locale define a convensão americana
+
+        System.out.println("Digite o seu nome: ");
+        nome = scanner.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade: ");
+        idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura: ");
+        altura = scanner.nextDouble();
+
+        System.out.println("Olá, " + nome + " " + sobrenome + ". Você tem " + idade + " anos de idade e a sua altura é de " + altura + "cm.");
     }
 }
