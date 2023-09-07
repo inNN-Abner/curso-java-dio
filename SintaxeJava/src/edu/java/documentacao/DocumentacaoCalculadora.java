bbc5b52eaae3a03f184fcd155cffd21ef2a232ae
inNN-Abner <abner.faculdade440@gmail.com>
2023-09-07 13:22:49 -0300

    A importância da documentação de um Método

---
+++ SintaxeJava/src/edu/java/documentacao/DocumentacaoCalculadora.java
@@ -0,0 +1,14 @@
+package edu.java.documentacao;
+
+public class DocumentacaoCalculadora {
+    
+    /**
+     * Este método é utilizado para somar dois numéros inteiros.
+     * @param numeroUm - Este é o primeiro parâmentro do método
+     * @param numeroDois - Este é o segundo parâmentro do método
+     * @return - int o resultado deste método é a soma dos dois números
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+}
