b8a28849a5aa9f9fd655698236fb5730bcb3d8ce
inNN-Abner <abner.faculdade440@gmail.com>
2023-09-07 11:13:06 -0300

    Hello World!

---
+++ SintaxeJava/src/edu/java/sintaxejava/HelloWorld.java
@@ -0,0 +1,6 @@
+package edu.java.sintaxejava;
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World! - Essa é a minha primeira classe Java");
+    }
+}
